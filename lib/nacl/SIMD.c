a339ac871d41bb5ad4a440313766d00dd2e3733f
Yuhui Zhao <yhzhao343@gmail.com>
2015-10-27 22:39:43 -0400

    Optimization:backup expf changes. It doesn't work.

--- lib/nacl/SIMD.c
+++ lib/nacl/SIMD.c
@@ -1,5 +1,22 @@
+#include <stdio.h>
 #include "SIMD.h"
 
+// PPB_Messaging* ppb_messaging_interface = NULL;
+// PPB_Var* ppb_var_interface = NULL;
+// PPB_Core* ppb_core_interface = NULL;
+// PP_Instance g_Instance;
+
+// ppb_messaging_interface =
+//   (PPB_Messaging*)(get_browser(PPB_MESSAGING_INTERFACE));
+// ppb_var_interface = (PPB_Var*)(get_browser(PPB_VAR_INTERFACE));
+// ppb_core_interface = (PPB_Core*)(get_browser(PPB_CORE_INTERFACE));
+//TODO:
+//epi: signed
+//m128i: int
+//m128:  float
+//cast doesn't change bits
+//x > y if larger 1111, if not larger 0000
+//just use signed integer
 v4sf exp_v4sf(v4sf x) {
     const v4sf magicBias = v4sf_set1(0x1.800000p+23f);
     const v4sf zeroCutoff = v4sf_set1(-0x1.9FE368p+6f); /* The smallest x for which expf(x) is non-zero */
@@ -8,6 +25,7 @@ v4sf exp_v4sf(v4sf x) {
     const v4sf minusLn2_hi = v4sf_set1(-0x1.62E430p-4f);
     const v4sf minusLn2_lo = v4sf_set1( 0x1.05C610p-32f);
     const v4sf plusInf = v4sf_GET_INFINITY();
+    fprintf(stderr, "helloWorld!\n");
 
     const v4sf c2 = v4sf_set1(0x1.FFFFFCp-2f);
     const v4sf c3 = v4sf_set1(0x1.55548Cp-3f);
@@ -20,8 +38,8 @@ v4sf exp_v4sf(v4sf x) {
     const v4ui defaultExponent = v4ui_set1(0x3F800000u);
 
     v4sf t = v4sf_macc(x, log2e, magicBias);
-    v4sf e1 = v4sf_slli(t, v4sf_set1(23));
-    v4sf e2 = e1;
+    v4si e1 = v4ui_slli((v4ui) t, v4sf_set1(23));
+    v4si e2 = e1;
     e1 = v4sf_min(v4sf_max(e1, minExponent), maxExponent);
     e2 = e2 - e1;
 
@@ -71,3 +89,21 @@ v4sf exp_v4sf(v4sf x) {
     // f = v4sf_blendv(x,f,v4sf_cmpeq(x,x));
     // return f;
 }
+
+// void PostMessage(const char* str) {
+//   if (NULL == str)
+//     return;
+//   if (NULL == ppb_messaging_interface)
+//     return;
+//   if (0 == g_Instance)
+//     return;
+
+//   fprintf(stdout, "%s\n", str);
+//   fflush(stdout);
+
+//   if (ppb_var_interface != NULL) {
+//     struct PP_Var var = ppb_var_interface->VarFromUtf8(str, strlen(str));
+//     ppb_messaging_interface->PostMessage(g_Instance, var);
+//     ppb_var_interface->Release(var);
+//   }
+// }
--- lib/nacl/SIMD.h
+++ lib/nacl/SIMD.h
@@ -1,14 +1,26 @@
 #pragma once
 #include <stdlib.h>
+// #include <stdio.h>
 #include <stdint.h>
+// #include <string.h>
 #include <sys/types.h>
-#include <math.h>
-#define V4UI_TRUE  (v4ui_set1(0xffffffff))
-#define V4UI_FALSE  (v4ui_set1(0x0))
-#define FLOAT_INFINITY (0x7f800000)
+
+// #include "ppapi/c/pp_errors.h"
+// #include "ppapi/c/pp_module.h"
+// #include "ppapi/c/pp_var.h"
+// #include "ppapi/c/ppb.h"
+// #include "ppapi/c/ppb_core.h"
+// #include "ppapi/c/ppb_instance.h"
+// #include "ppapi/c/ppb_messaging.h"
+// #include "ppapi/c/ppb_var.h"
+// #include "ppapi/c/ppp.h"
+// #include "ppapi/c/ppp_instance.h"
+// #include "ppapi/c/ppp_messaging.h"
+// #include "error_handling/error_handling.h"
+
 typedef float v4sf __attribute__((__vector_size__(16)));
 typedef unsigned int v4ui __attribute__((__vector_size__(16)));
-// typedef unsigned __int128 u_v4sf_128;
+
 inline v4sf v4sf_load(const float* a) {
     return *((const v4sf*)a);
 }
@@ -22,96 +34,49 @@ inline v4sf v4sf_set1(float a) {
 }
 
 inline v4ui v4ui_set1(unsigned int a) {
-    return (v4sf) {a, a, a, a};
-}
-
-inline v4sf v4sf_set(float a, float b, float c, float d) {
-    return (v4sf) {a, b, c, d};
+    return (v4ui) {a, a, a, a};
 }
 
 inline v4sf v4sf_macc(v4sf x, v4sf log2e, v4sf magicBias) {
     return x * log2e + magicBias;
 }
 
-inline v4sf v4sf_and(v4sf x, v4ui y) {
-    v4ui ans = (*((v4ui *)(&x)) & *((v4ui *)(&y)));
-    return *((v4sf*) &ans);
+inline v4sf v4sf_and(v4sf x, v4sf y) {
+    return (v4sf) (((v4ui) x) & ((v4ui) y));
 }
 
 inline v4sf v4sf_andnot(v4sf x, v4ui y) {
-    v4ui ans = ~(*((v4ui *)(&x)) & *((v4ui *)(&y)));
-    return *((v4sf*) &ans);
-}
-
-inline v4sf v4sf_max(v4sf x, v4sf y) {
-    v4ui xLarger = x > y;
-    v4ui ans = (*((v4ui *)(&x)) & xLarger) | (*((v4ui *)(&x)) & ~xLarger);
-    return *((v4sf*) &ans);
-}
-
-inline v4sf v4sf_min(v4sf x, v4sf y) {
-    v4ui xSmaller = x < y;
-    v4ui ans = (*((v4ui *)(&x)) & xSmaller) | (*((v4ui *)(&x)) & ~xSmaller);
-    return *((v4sf*) &ans);
-}
-
-inline v4sf v4sf_GET_INFINITY() {
-    v4ui ans = v4ui_set1(FLOAT_INFINITY);
-    return *((v4sf *)(& ans));
-}
-
-inline v4sf v4sf_slli(v4sf x, v4sf y) {
-    unsigned int* x_ptr = (unsigned int*) &x;
-    unsigned int* y_ptr = (unsigned int*) &y;
-    return (v4sf) {x_ptr[0] << y_ptr[0], x_ptr[1] << y_ptr[1], x_ptr[2] << y_ptr[2], x_ptr[3] << y_ptr[3]};
-    //The code below doesn't work for some reason...
-    // v4ui ans = (*((v4ui *)(&x)) << *((v4ui *)(&y)));
-    // return *((v4sf*) &ans);
-    //
-    //Error Message:
-    //Not handled: float undef
-    //LLVM ERROR: FlattenGlobals: Constant type not handled for reloc
-    //ninja: build stopped: subcommand failed.
-
-}
-
-inline v4sf v4sf_permute2(v4sf x, v4sf y, v4sf control) {
-    float* x_ptr = (float*) &x;
-    float* y_ptr = (float*) &y;
-    v4ui mask = v4ui_set1(0x7);
-    v4ui sel = *((v4ui*) &control) & mask;
-    int * sel_ptr = (int*) &sel;
-    int a = *sel_ptr;
-    int b = *(sel_ptr+1);
-    int c = *(sel_ptr+2);
-    int d = *(sel_ptr+3);
-    return (v4sf) {a < 4 ? *(x_ptr + a) : *(y_ptr + a - 4)
-                  ,b < 4 ? *(x_ptr + b) : *(y_ptr + b - 4)
-                  ,c < 4 ? *(x_ptr + c) : *(y_ptr + c - 4)
-                  ,d < 4 ? *(x_ptr + d) : *(y_ptr + d - 4)};
-}
-
-inline v4sf v4sf_cmplt(v4sf x, v4sf y) {
-    v4ui xSmaller = x < y;
-    v4ui ans = (V4UI_TRUE & xSmaller) | (V4UI_FALSE & ~xSmaller);
-    return *((v4sf*) &ans);
-}
-
-inline v4sf v4sf_cmpgt(v4sf x, v4sf y) {
-    v4ui xLarger = x > y;
-    v4ui ans = (V4UI_TRUE & xLarger) | (V4UI_FALSE & ~xLarger);
-    return *((v4sf*) &ans);
-}
-
-inline v4sf v4sf_cmpeq(v4sf x, v4sf y) {
-    v4ui xEqualsY = x == y;
-    v4ui ans = (V4UI_TRUE & xEqualsY) | (V4UI_FALSE & ~xEqualsY);
-    return *((v4sf*) &ans);
+    return (v4sf) (~((v4ui) x) & ((v4ui) y));
 }
 
 inline v4sf v4sf_blendv(v4sf x, v4sf y, v4sf mask) {
-    v4ui mask_isPos = (*((v4ui *)(&mask)) > V4UI_FALSE);
-    v4ui ans = ((*(v4ui *)(&x)) & mask_isPos) | ((*(v4ui *)(&y)) & ~mask_isPos);
-    return *((v4sf*) &ans);
+    v4ui imask = (v4ui) mask;
+    return (v4sf) ((((v4ui) x) & ~imask) | (((v4ui) y) & imask));
 }
+
+inline v4sf v4sf_max(v4sf x, v4sf y) {
+    return v4sf_blendv(x, y, y > x);
+}
+
+inline v4sf v4sf_min(v4sf x, v4sf y) {
+    return v4sf_blendv(x, y, x > y);
+}
+
+inline v4sf v4sf_GET_INFINITY() {
+    return v4sf_set1(__builtin_inff());
+}
+
+inline v4sf v4sf_cmplt(v4sf x, v4sf y) {
+    return x < y;
+}
+
+inline v4sf v4sf_cmpgt(v4sf x, v4sf y) {
+    return x > y;
+}
+
+inline v4sf v4sf_cmpeq(v4sf x, v4sf y) {
+    return x == y;
+}
+
 v4sf exp_v4sf(v4sf x);
+// void PostMessage(const char* str);
